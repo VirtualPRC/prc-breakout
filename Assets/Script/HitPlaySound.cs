069d2a9cbb60d83160d1d7729a917ee5a83666d4
Kadzu <jackolantern1228@gmail.com>
2018-05-26 23:15:45 +0900

    ＢＧＭの追加

---
+++ Assets/Script/HitPlaySound.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class HitPlaySound : MonoBehaviour
+{
+    public AudioClip sound;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        AudioSource.PlayClipAtPoint(sound, transform.position);
+    }
+}
