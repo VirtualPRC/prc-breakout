c467122402f4208ee3754360c36fefebfb24783e
Kadzu <jackolantern1228@gmail.com>
2018-05-27 00:11:27 +0900

    徐々にボールが加速していく機能を追加

--- Assets/Script/BallController.cs
+++ Assets/Script/BallController.cs
@@ -3,10 +3,22 @@
 public class BallController : MonoBehaviour
 {
     public float speed = 10;
+    public float speedUpRate = 1.1f;
+    private int collisionCount;
 
     private void Start()
     {
         var force = (transform.forward + transform.right) * speed;
         GetComponent<Rigidbody>().AddForce(force, ForceMode.VelocityChange);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        collisionCount += 1;
+        if (collisionCount > 10)
+        {
+            GetComponent<Rigidbody>().velocity = GetComponent<Rigidbody>().velocity * speedUpRate;
+            collisionCount = 0;
+        }
     }
 }
