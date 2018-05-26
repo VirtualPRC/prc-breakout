5f6453fe799fa0ea5af445c71594c3a58c743320
yoku001 <yoku001@users.noreply.github.com>
2018-05-26 22:55:39 +0900

    Add SE/BGM

---
+++ Assets/BottomWallController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottomWallController : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    /// <summary>
+    /// 衝突時の処理
+    /// </summary>
+    /// <param name="collision"> 衝突オブジェクト </param>
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(collision.gameObject);
+    }
+}
---
+++ Assets/HitPlaySound.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitPlaySound : MonoBehaviour
+{
+    /// <summary>
+    /// 衝突効果音
+    /// </summary>
+    public AudioClip sound;
+
+    /// <summary>
+    /// 衝突時の処理
+    /// </summary>
+    /// <param name="collision"> 衝突オブジェクト </param>
+    private void OnCollisionEnter(Collision collision)
+    {
+        AudioSource.PlayClipAtPoint(sound, transform.position);
+    }
+}
