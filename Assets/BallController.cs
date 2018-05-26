aed6b39e0fb17b5f4c73a80579e052c306a5ab32
yoku001 <yoku001@users.noreply.github.com>
2018-05-26 22:22:53 +0900

    Make paddle movement

---
+++ Assets/BallController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    /// <summary>
+    /// 初速
+    /// </summary>
+    public float speed = 10;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        // 初速を設定
+        var force = (this.transform.forward + this.transform.right) * speed;
+        GetComponent<Rigidbody>().AddForce(force, ForceMode.VelocityChange);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/PaddleController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour
+{
+    /// <summary>
+    /// 加速度
+    /// </summary>
+    public float accel = 1000;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var force = transform.right * Input.GetAxisRaw("Horizontal") * accel;
+        GetComponent<Rigidbody>().AddForce(force, ForceMode.Impulse);
+    }
+}
